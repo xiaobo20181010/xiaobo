e461b8846fce24cd2271da7ab6ff7a7a5d431a20
XIAO BO <xiaob@garena.com>
2018-10-12 10:56:49 +0800

    init files

---
+++ home_application/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns
+
+urlpatterns = patterns(
+    'home_application.views',
+    (r'^$', 'home'),
+    (r'^dev-guide/$', 'dev_guide'),
+    (r'^contactus/$', 'contactus'),
+    (r'^helloworld/$', 'helloworld'),
+)
